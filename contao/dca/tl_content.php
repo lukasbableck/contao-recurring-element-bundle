3dc223a4a8df75b1ef087a022bd675506369a36f
Lukas Bableck <lukas.bableck@netprofit.de>
2024-01-30 12:17:23 +0100

    add fields via onload_callback to provide compatibility with rocksolid custom elements

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -69,14 +69,6 @@ $GLOBALS["TL_DCA"]["tl_content"]["fields"]["recurringMonths"] = [
 	"sql" => "blob NULL",
 ];
 
-$palette = "{recurring_legend},recurring;";
-
-foreach($GLOBALS["TL_DCA"]["tl_content"]["palettes"] as $key => $value){
-	if($key == "__selector__"){
-		continue;
-	}
-	$GLOBALS["TL_DCA"]["tl_content"]["palettes"][$key] = str_replace("{expert_legend:hide}", $palette . "{expert_legend:hide}", $value);
-}
 
 $GLOBALS["TL_DCA"]["tl_content"]["palettes"]["__selector__"][] = "recurring";
 $GLOBALS["TL_DCA"]["tl_content"]["subpalettes"]["recurring"] = "recurringInterval,recurringIntervalCount,recurringIntervalDuration,recurringStart,recurringEnd,recurringWeekdays,recurringMonthdays,recurringMonths";
---
+++ src/EventListener/DataContainer/AddRecurringFieldsCallback.php
@@ -0,0 +1,32 @@
+<?php
+namespace Lukasbableck\ContaoRecurringElementBundle\EventListener\DataContainer;
+
+use Contao\ContentModel;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
+use Contao\DataContainer;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+#[AsCallback(table: 'tl_content', target: 'config.onload')]
+class AddRecurringFieldsCallback{
+
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack){
+        $this->requestStack = $requestStack;
+    }
+
+    public function __invoke(DataContainer $dc = null): void{
+        if (null === $dc || !$dc->id || 'edit' !== $this->requestStack->getCurrentRequest()->query->get('act')) {
+            return;
+        }
+
+		echo "test";
+		foreach($GLOBALS["TL_DCA"]["tl_content"]["palettes"] as $key => $value){
+			if($key == "__selector__"){
+				continue;
+			}
+			$GLOBALS["TL_DCA"]["tl_content"]["palettes"][$key] = str_replace("{expert_legend:hide}", "{recurring_legend},recurring;{expert_legend:hide}", $value);
+		}
+
+    }
+}
